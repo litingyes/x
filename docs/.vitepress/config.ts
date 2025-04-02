6b8ef4ce548c238343d8745f4f54b6d78340d761
litingyes <luz.liting@gmail.com>
2025-04-02 22:05:23 +0800

    chore: init

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  title: "X",
+  description: "Coding & Life",
+  themeConfig: {
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  }
+})
