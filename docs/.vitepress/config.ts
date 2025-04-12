cbc91162009b514fbce47972e2febec950c8c2c8
litingyes <luz.liting@gmail.com>
2025-04-12 13:13:50 +0800

    chore: update basic config

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,10 +2,13 @@ import { defineConfig } from 'vitepress'
 
 export default defineConfig({
   title: "X",
+  titleTemplate: 'X',
+  lang: 'zh-CN',
   description: "Coding & Life",
+  lastUpdated: true,
   themeConfig: {
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+      { icon: 'github', link: 'https://github.com/litingyes/x' }
     ]
   }
 })
