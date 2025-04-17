e2d61791c52e376d9786ec4bbd316325c8a31c81
litingyes <luz.liting@gmail.com>
2025-04-17 22:03:04 +0800

    ci(eslint): detect docs dir

--- eslint.config.js
+++ eslint.config.js
@@ -3,4 +3,7 @@ import antfu from '@antfu/eslint-config'
 export default antfu({
   formatters: true,
   vue: true,
+  typescript: true,
+}, {
+  files: ['docs/**/*.ts', 'docs/**/*.vue', 'docs/**/*.md'],
 })
